56155dbd1ab3c7ada7685e44365868ef8717eba3
fahadbgnr <aminulislamfahad1@gmail.com>
2025-02-05 23:57:42 +0600

    i learn js if variable

---
+++ if.js
@@ -0,0 +1,22 @@
+/**
+ **********************
+ *     STRUCTURE
+ * if (condition){
+ *       execute code if condition above is ture
+ * }
+ * 
+ * else {
+ *     execute code if the condition is false
+ * }
+ * 
+ *  
+ * */
+
+ var weight = 40
+
+ if(weight < 20){
+    console.log('i will carry with myself');
+ }
+ else{
+    console.log('i will rent a rickshaw')
+ }
